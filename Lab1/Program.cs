fc5279a9b8b450f9b495772fd003a5d38ad3ac12
Maksym <maksimgunavyj9@gmail.com>
2022-09-02 14:51:57 +0300

    second commit

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, world!");
+            Console.WriteLine("10+2");
             Console.ReadLine();
         }
     }
